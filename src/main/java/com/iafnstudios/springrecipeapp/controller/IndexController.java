a3eba7478a8e009a56b1b6987c7e1d24e8072cfa
sen-abdullah@yandex.com <sen-abdullah@yandex.com>
2019-11-04 08:47:23 +0300

    Initial commit

---
+++ src/main/java/com/iafnstudios/springrecipeapp/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.iafnstudios.springrecipeapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","/index","/index.html"})
+    public String getIndexPage(){
+        return "index";
+    }
+
+}
